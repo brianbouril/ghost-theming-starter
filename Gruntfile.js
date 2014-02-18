761148430dd16926549186114479b1917941a948
Brian Bouril <brian@brianbouril.com>
2014-02-17 22:46:45 -0600

    started adding customsetup task for first time use

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,12 @@ module.exports = function(grunt) {
         'dist/css/*'
         ],
         tasks: ['cssmin']
+      },
+      ghostcss: {
+        files: [
+        '<%= ghost_theme_location %>/assets/css/*'
+        ],
+        tasks: ['reload']
       }
     },
 
@@ -26,7 +32,7 @@ module.exports = function(grunt) {
       },
       combinejs: {
         files: {
-          '<%= ghost_location %>content/themes/<%= ghost_theme_name %>/assets/js/all.min.js': 
+          '<%= ghost_theme_location %>/assets/js/all.min.js': 
           [
           'bower_components/modernizr/modernizr.js',
           'custom_components/responsive_iframes/responsive_iframes.js'
@@ -50,13 +56,12 @@ module.exports = function(grunt) {
     cssmin: {
       combine: {
         files: {
-          '<%= ghost_location %>content/themes/<%= ghost_theme_name %>/assets/css/style.css': ['bower_components/normalize-css/normalize.css', 'dist/css/main.css']
+          '<%= ghost_theme_location %>/assets/css/style.css': ['bower_components/normalize-css/normalize.css', 'dist/css/main.css']
         }
       }
     },
 
-    'ghost_location': '../Ghost/',
-    'ghost_theme_name': 'solo',
+    'ghost_theme_location': '<%= pkg.ghostinstall %>content/themes/<%= pkg.name %>'
 
   });
 
@@ -67,5 +72,31 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', ['watch']);
+
+  grunt.registerTask('setup', 'Create initial theme directory and files in Ghost/content/themes', function() {
+    // if (failureOfSomeKind) {
+    //   grunt.log.error('This is an error message.');
+    // }
+
+    //grunt.file.exists(path1 [, path2 [, ...]])
+    // Fail by returning false if this task had errors
+    //if (ifErrors) { return false; }
+    
+    var package = grunt.file.readJSON('package.json');
+    grunt.log.writeln("Theme name is: "+package.name);
+    grunt.log.writeln("Your theme is located at: "+package.ghostinstall + "content/themes/" + package.name);
+
+    //grunt.log.writeln('This is the success message');
+  });
+
+  // https://coderwall.com/p/keuhda
+  grunt.registerTask("reload", "reload Chrome on OS X",
+    function() {
+      require("child_process").exec("osascript " +
+          "-e 'tell application \"Google Chrome\" " +
+            "to tell the active tab of its first window' " +
+          "-e 'reload' " +
+          "-e 'end tell'");
+  });
 
 };
